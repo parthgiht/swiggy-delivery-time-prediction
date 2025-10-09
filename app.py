331b04df652fe2ad4e69fc6f452754dcd3abe247
Parth Patel <parthppa7931@gmail.com>
2025-10-09 11:48:30 +0530

    minor changes

--- app.py
+++ app.py
@@ -147,5 +147,5 @@ def do_predictions(data: Data):
     return predictions
    
    
-if __name__ == "__main__":
-    uvicorn.run(app="app:app", host = "0.0.0.0", port = 8000)
+# if __name__ == "__main__":
+uvicorn.run(app="app:app", host = "0.0.0.0", port = 8000)
