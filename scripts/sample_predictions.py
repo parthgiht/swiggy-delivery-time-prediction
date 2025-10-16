d6fe81c524d0ef3173e71715b572909fad04d6f7
Parth Patel <parthppa7931@gmail.com>
2025-10-16 20:02:48 +0530

    changes in sample prediction

--- scripts/sample_predictions.py
+++ scripts/sample_predictions.py
@@ -11,10 +11,10 @@ predict_url = "http://127.0.0.1:8000/predict"
 
 # sample row for testing the endpoint
 sample_row = pd.read_csv(data_path).dropna().sample(1)
-print("The target value is", sample_row.iloc[:,-1].values.item().replace("(min) ",""))
+print("The target value is", str(sample_row.iloc[:,-1].values.item()).replace("(min) ",""))
     
 # remove the target column
-data = sample_row.drop(columns=[sample_row.columns.tolist()[-1]]).squeeze().to_dict()
+data = sample_row.drop(columns=[sample_row.columns.tolist()[-1]]).iloc[0].to_dict()
 print(data)
 
 # get the response from API
@@ -23,6 +23,7 @@ response = requests.post(url=predict_url,json=data)
 print("The status code for response is", response.status_code)
 
 if response.status_code == 200:
-    print(f"The prediction value by the API is {float(response.text):.2f} min")
+    prediction = response.json()["predicted_delivery_time"]
+    print(f"The prediction value by the API is {float(prediction):.2f} min")
 else:
     print("Error:", response.status_code)
